02b816dfde72571699e736eae74ad032e6a61ea4
Ramon54xg <ramontrinity@outlook.com>
2018-10-05 21:18:52 -0300

    Add files via upload

---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package Cadastrar.fxml;
+
+import javafx.event.ActionEvent;
+
+
+public class Controller {
+
+    public void onButtonClick(ActionEvent event) {
+        System.out.println("Clique no botão");
+    }
+
+}
